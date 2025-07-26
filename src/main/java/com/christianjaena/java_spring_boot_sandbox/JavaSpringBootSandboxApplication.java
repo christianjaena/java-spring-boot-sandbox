b1dec4b9c29ceae48cb3ba41c3a19554dc561012
Christian Jaena <jaenachristian@gmail.com>
2025-07-26 13:51:57 +0800

    Initial commit

---
+++ src/main/java/com/christianjaena/java_spring_boot_sandbox/JavaSpringBootSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.christianjaena.java_spring_boot_sandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootSandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringBootSandboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/christianjaena/java_spring_boot_sandbox/JavaSpringBootSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.christianjaena.java_spring_boot_sandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBootSandboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
